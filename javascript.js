3a0aac5a33760d973754872e5012f8f120e32ee2
derryck1 <derryckadu@gmail.com>
2022-08-20 21:24:21 -0400

    Will let us make a grid based on row/column input
    This commit adds the code that lets make a grid.
    There is a grid container, that contains a row container based on the
    amount of rows entered earlier, that also containts square divs based
    on the amount of columns entered. My css stylings were done through the
    DOM

--- javascript.js
+++ javascript.js
@@ -1,30 +1,39 @@
 //Links the HTML Div Class to the Javascript file
-const grid = document.querySelector (".Grid")
-
-
-
-
-let rowAmount = prompt("How many rows?", 1);
-let columnAmount = prompt("How many columns?", 1)
+const grid = document.querySelector (".grid")
 
 
 let createGrid = function () {
-    let squareAmount = rowAmount * columnAmount;
+    let rowAmount = prompt("How many rows?", 1);
+    let columnAmount = prompt("How many columns?", 1)
 
-    for (let i = 1; i <= squareAmount; i++) {
-        const square = document.createElement ("div")
-        square.setAttribute ("id", "square" + i)
-        grid.append(square)
+
+    for (i = 1; i <= rowAmount; i++) {
+        const row = document.createElement ("div")
+        row.setAttribute  ("id", "row" + i)
+        row.style.cssText = "display:flex; margin: 1px; gap: 1px;"
+        grid.append(row);
+
+        for (let i = 1; i <= columnAmount; i++) {
+            const square = document.createElement ("div")
+            square.setAttribute ("id", "square" + i)
+            square.style.cssText  = "height: 20px; width: 20px; border:solid;"
+            row.append(square)
     }
+    
+
+ }
+
+
+
+
+
  }
   
  createGrid();
 
 
-//Append first square of grid
-//If i < squaure amount
- //add the ID attribute and properly name the ID attribute
-//append newly created square to Grid
+// create first amount of grids based on amount of columns
+//skip to second row after column amount has been reache d.
 
 
 
